aa78d178fd72dcdb7447518f0dfb2e0b676c675c
J1W0N-1209 <nabook754@naver.com>
2023-08-19 18:35:20 +0900

    init

---
+++ src/main/java/com/project/insert/InsertApplication.java
@@ -0,0 +1,13 @@
+package com.project.insert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsertApplication.class, args);
+	}
+
+}
