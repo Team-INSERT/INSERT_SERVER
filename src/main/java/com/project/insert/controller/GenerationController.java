383d7bcbdbe8869a42fa5f62d7b1a20298550370
J1W0N-1209 <nabook754@naver.com>
2023-08-21 00:21:58 +0900

    feat : getMember API contorller 추가

--- src/main/java/com/project/insert/controller/GenerationController.java
+++ src/main/java/com/project/insert/controller/GenerationController.java
@@ -1,20 +1,23 @@
 package com.project.insert.controller;
 
 import com.project.insert.controller.dto.SaveGenerationDto;
-import com.project.insert.repository.GenerationRepository;
 import com.project.insert.service.GenerationService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 public class GenerationController {
     private final GenerationService generationService;
-
     @PostMapping("/api/member/generation")
     public String createGeneration(@RequestBody SaveGenerationDto requestDto){
         return generationService.save(requestDto);
+    }
+
+    @GetMapping("/api/member/{team}")
+    @ResponseBody
+    public JSONArray getMemberFront(@PathVariable String team) throws Exception {
+        return generationService.getMember(team);
     }
 }
